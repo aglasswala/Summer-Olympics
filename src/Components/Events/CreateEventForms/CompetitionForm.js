305bd76d6caee1ef61f41846ebbaa483487f8e9c
Aadil Glasswala <aglasswala@gmail.com>
2019-04-03 13:39:22 -0400

    added stadiums and locations

--- src/Components/Events/CreateEventForms/CompetitionForm.js
+++ src/Components/Events/CreateEventForms/CompetitionForm.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { withStyles, TextField } from '@material-ui/core'
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, TimePicker, DatePicker } from 'material-ui-pickers';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const competitionFormstyles = {
   wrapper: {
@@ -20,37 +23,83 @@ const competitionFormstyles = {
   }
 }
 
+const stadiums = ["Carioca Arena 1", "Carioca Arena 2", "Carioca Arena 3", "Olympic Aquatics Stadium", "Deodoro Olympic Whitewater Stadium"]
+
+const locations = ["Barra da Tijuca,  Rio de Janeiro, Brazil", "Barra Olympic Park in Rio de Janeiro, Brazil", "Deodoro, Rio de Janeiro, Brazil"]
+
 class CompetitionForm extends Component {
+
+  state = {
+    time: new Date(),
+    stadium: "",
+    location: "",
+    date: new Date()
+  }
+
+  onTimeChange = (date) => {
+    this.setState({ time: date })
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
 
   render() {
     const { classes } = this.props
+    console.log(this.state)
     return (
-      <form className={classes.form}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <span className={classes.wrapper}>
-          <TextField 
-            label="Event Name"
-            className={classes.textField}
+          <TimePicker
+            margin="normal"
+            label="Time picker"
+            value={this.state.time}
+            onChange={this.onTimeChange}
+          />
+          <DatePicker
+            margin="normal"
+            label="Date picker"
+            value={this.state.date}
+            onChange={this.onTimeChange}
           />
         </span>
         <span className={classes.wrapper}>
           <TextField 
-            label="Event Name"
+            id="stadium"
+            label="Stadium"
             className={classes.textField}
-          />
+            select
+            value={this.state.stadium}
+            onChange={this.handleChange("stadium")}
+            margin="normal"
+          >
+            {stadiums.map((stadium, key) => (
+              <MenuItem key={key} value={stadium}>
+                {stadium}
+              </MenuItem>
+            ))}
+          </TextField>
         </span>
         <span className={classes.wrapper}>
           <TextField 
-            label="Event Name"
+            id="location"
+            label="Location"
             className={classes.textField}
-          />
+            select
+            value={this.state.location}
+            onChange={this.handleChange("location")}
+            margin="normal"
+          >
+            {locations.map((location, key) => (
+              <MenuItem key={key} value={location}>
+                {location}
+              </MenuItem>
+            ))}
+          </TextField>
         </span>
-        <span className={classes.wrapper}>
-          <TextField 
-            label="Event Name"
-            className={classes.textField}
-          />
-        </span>
-      </form>
+      </MuiPickersUtilsProvider>
     )
   }
 }
