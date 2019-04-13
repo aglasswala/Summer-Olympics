813fda49fd43936ed890d055a54f2b9dbe9233b1
Aadil Glasswala <aglasswala@gmail.com>
2019-04-12 21:07:43 -0400

    updated user log out

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -225,20 +225,13 @@ const Sidebar = ({...props}) => {
                             />
                           </ListItem>
                         </NavLink>
-                        <NavLink
-                          to={"/"}
-                          exact
-                          className={classes.item}
-                          activeClassName="active"
-                        >
-                          <ListItem button className={classes.itemLink} onClick={logoutUser}>
-                            <ListItemText
-                              primary={"Logout"}
-                              className={classes.itemText}
-                              disableTypography={true}
-                            />
-                          </ListItem>
-                        </NavLink>
+                        <ListItem button className={classes.itemLink} onClick={logoutUser}>
+                          <ListItemText
+                            primary={"Logout"}
+                            className={classes.itemText}
+                            disableTypography={true}
+                          />
+                        </ListItem>
                       </List>
                     </div>
                     <div
