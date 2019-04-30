c21d3076534c82efe423c6effe16f8e4a8d710c9
Aadil Glasswala <aglasswala@gmail.com>
2019-04-30 11:30:42 -0400

    updated route'

--- src/views/HomePage/Medalists.js
+++ src/views/HomePage/Medalists.js
@@ -83,7 +83,7 @@ class Medalists extends Component {
   }
 
   componentDidMount() {
-    fetch("https://summer-olympics.herokuapp.com//api/getAllMedalists")
+    fetch("https://summer-olympics.herokuapp.com/api/getAllMedalists")
       .then(response => response.json())
       .then(result => this.setState({ allCereEvents: result }))
       .catch(err => {
